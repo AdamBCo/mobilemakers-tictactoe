67c480c802bfdb30d07470a12e32a6fa570f92f2
jhmcclellandii <jhmcclellandii@gmail.com>
2015-05-17 16:01:51 -0500

    still sucking on for in loops

--- mobilemakers-tictactoe/ViewController.m
+++ mobilemakers-tictactoe/ViewController.m
@@ -15,7 +15,7 @@
 @property NSMutableArray *winStateArray;
 
 @property int goNumber;
-@property NSInteger winner;
+@property NSObject *winner;
 
 
 @end
@@ -27,15 +27,14 @@
     [super viewDidLoad];
     self.goNumber = 0;
     self.winner = 0;
-    self.gameStateArray = [NSMutableArray new];
+    self.gameStateArray = [[NSMutableArray alloc]init];
     self.gameStateArray = [NSMutableArray arrayWithObjects:@0,@0,@0,@0,@0,@0,@0,@0,@0, nil];
-
-    self.winStateArray = [NSMutableArray new];
-    self.winStateArray = [NSMutableArray arrayWithObjects:
+    self.winStateArray  = [NSMutableArray new];
+    self.winStateArray  = [NSMutableArray arrayWithObjects:
                           [NSMutableArray arrayWithObjects:@[@0,@1,@2], nil],
                           [NSMutableArray arrayWithObjects:@[@3,@4,@5], nil],
                           [NSMutableArray arrayWithObjects:@[@6,@7,@8], nil],
-                          [NSMutableArray arrayWithObjects:@[@0,@3,@6], nil],
+                          [NSMutableArray arrayWithObjects:@[@0,@2,@6], nil],
                           [NSMutableArray arrayWithObjects:@[@1,@4,@7], nil],
                           [NSMutableArray arrayWithObjects:@[@2,@5,@8], nil],
                           [NSMutableArray arrayWithObjects:@[@0,@4,@8], nil],
@@ -49,7 +48,7 @@
     if (self.goNumber % 2 == 0) {
         [self.gameStateArray replaceObjectAtIndex:[sender tag] withObject:@1];
     } else {
-        [self.gameStateArray replaceObjectAtIndex:[sender tag] withObject:@2];
+        [self.gameStateArray replaceObjectAtIndex:[sender tag] withObject:@2]; //for now, but change later
     }
     self.goNumber = self.goNumber + 1;
 
@@ -58,17 +57,29 @@
 - (IBAction)testButton:(id)sender {
     NSLog(@"%@",self.gameStateArray);
 
+//    //test for loop
+//    for (int i = 1; i <= 10; i++) {
+//        NSLog(@"%d",i);
+//    }
 
+//    if (self.gameStateArray[0] == self.gameStateArray[1] && self.gameStateArray[1] == self.gameStateArray[2] && self.gameStateArray[0] != 0) {
+//        NSLog(@"fuck yeah");
+//    } else {
+//        NSLog(@"meh");
+//    }
 
-    //test for loop
-    for (int i = 1; i <= 10; i++) {
-        NSLog(@"%d",i);
+    for (NSObject *ugh in self.winStateArray) {
+        if (self.gameStateArray[(int)self.winStateArray[0]] == self.gameStateArray[(int)self.winStateArray[1]] && self.gameStateArray[(int)self.winStateArray[1]] == self.gameStateArray[(int)self.winStateArray[2]] && self.gameStateArray[(int)self.winStateArray[0]] != 0) {
+            NSLog(@"%@",ugh);
+        } else {
+            NSLog(@"failwhale");
+        }
+
     }
 
-    //test for in loop
-    int derp = (int)self.gameStateArray[1];
-    NSLog(@"%i",derp);
-}
 
+
+
+}
 
 @end
